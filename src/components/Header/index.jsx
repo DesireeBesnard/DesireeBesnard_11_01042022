2205817499b981ec32611718fbb45faa4e818c59
DesireeBesnard <desiree.besnard@gmail.com>
2022-04-05 17:04:08 +0200

    banner with background image and filter

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import '../../style/Header.css'
 function Header() {
     return (
         <div className='container header'>
-            <Link to ='/' ><img src={require('../../assets/logoHeader.png')} alt='logo' className='logo' /></Link>
+            <Link to ='/' ><img src='./assets/logoHeader.png' alt='logo' className='logo' /></Link>
             <nav>
                 <NavLink to='/' className={({ isActive }) => isActive ? "link-active" : "link"}>Accueil</NavLink>
                 <NavLink to='/about' className={({ isActive }) => isActive ? "link-active" : "link"}>A Propos</NavLink>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,14 @@
-import logo from '../../logo.svg';
+import '../../style/index.css';
 import '../../style/Home.css';
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        > 
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='container'>
+      <section className='banner home-banner'>
+        <div className="overlay"></div>
+        <h1>Chez vous, partout et ailleurs</h1>
+      </section>
+    </main>
   );
 }
 
