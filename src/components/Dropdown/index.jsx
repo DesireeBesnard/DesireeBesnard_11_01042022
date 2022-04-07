8906085de712bf9ef0419be0b4fb7f1065e8c180
DesireeBesnard <desiree.besnard@gmail.com>
2022-04-07 16:43:55 +0200

    toggle collapse icon

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -3,10 +3,19 @@ import '../../style/Dropdown.css'
 
 function Dropdown(props) {
 
+    const dropdownIcon = document.querySelector('.toggleDropdown')
+    const content = document.querySelector('.dropdownContent')
+
+    const handleToggle = (e) => {
+
+        e.target.classList.toggle('open')
+        // content.classList.toggle('display')
+    }
+
     return (
         <div className='dropdown'>
             <h2>{props.title}</h2>
-            <img className='dropdownIcon' src={process.env.PUBLIC_URL + '/assets/dropdown.png'} alt="dropdown icon" />
+            <button className='toggleDropdown' onClick={handleToggle}><img className='dropdownIcon' src={process.env.PUBLIC_URL + '/assets/dropdown.png'} alt="dropdown icon" /></button>
             <div className='dropdownContent'>
                 {props.content}
             </div>
